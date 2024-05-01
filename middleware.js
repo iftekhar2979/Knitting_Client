89f7d03b40a669f61ac23b087d870e202ff7e4a1
ifti2979 <salminifti79@gmail.com>
2024-05-01 23:10:29 +0600

    url changed

--- middleware.js
+++ middleware.js
@@ -6,10 +6,7 @@ const middleware = (req) => {
     let verify = req.cookies.get("jwt")
     let url = req.url
     if (!verify && url.includes("/dashboard")) {
-        const protocol = req.headers['x-forwarded-proto'] || 'http'; // Get the protocol (HTTP or HTTPS)
-        const host = req.headers['host']; // Get the hostname
-        const loginUrl = `${protocol}://${host}/login`; // Construct the full login URL
-        return NextResponse.redirect(loginUrl);
+        return NextResponse.redirect(`https://knitting-client-8bq3.vercel.app/login`);
     }
 
 };
