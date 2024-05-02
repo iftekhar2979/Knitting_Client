2178bcbf8acd60232466ef4889f37bb144d46d05
ifti2979 <salminifti79@gmail.com>
2024-05-02 21:26:56 +0600

    another changes in middleware

--- middleware.js
+++ middleware.js
@@ -1,10 +1,10 @@
 import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
 import react from 'react';
 
 const middleware = (req) => {
     const cookieStore = cookies();
     const verify = cookieStore.get('jwt');
-    // console.log(verify.value)
     let url = req.url
     if (!verify && url.includes("/dashboard")) {
         return NextResponse.redirect(`https://knitting-client-8bq3.vercel.app/login`);
