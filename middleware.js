bbde23bf232e97190aa823f7d62ad4f47aa94b4b
ifti2979 <salminifti79@gmail.com>
2024-05-02 23:12:05 +0600

    consoled middleware

--- middleware.js
+++ middleware.js
@@ -3,12 +3,13 @@ import { NextResponse } from 'next/server';
 import react from 'react';
 
 const middleware = (req) => {
+    console.log(req)
     const cookieStore = cookies();
+    console.log(cookieStore)
     const verify = cookieStore.get('jwt');
     let url = req.url
     if (!verify && url.includes("/dashboard")) {
         return NextResponse.redirect(`https://knitting-client-8bq3.vercel.app/login`);
-        // return NextResponse.redirect(`http://localhost:3000/login`);
     }
 
 };
